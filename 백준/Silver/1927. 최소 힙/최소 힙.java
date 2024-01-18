00c17b16767d435994bc69ae69b2300224d0848c
TaeHoon0 <48711163+TaeHoon0@users.noreply.github.com>
2024-01-18 09:24:40 +0900

    [Silver II] Title: 최소 힙, Time: 1076 ms, Memory: 33960 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	// 최소 힙을 사용하는 문제입니다.
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 자바에서 힙을 구현하기 위해 우선순위 큐를 사용
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for(int i = 0; i < N; i++){
+			int x = Integer.parseInt(br.readLine());
+
+			if(x > 0) maxHeap.add(x);
+			else{
+				if(maxHeap.isEmpty())
+					System.out.println(0);
+				else
+					System.out.println(maxHeap.poll());
+			}
+
+		}
+	}
+
+}
