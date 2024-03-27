9fa33048ac6f6d22ba6b32cb370ec8ffe9f9cd91
TaeHoon0 <48711163+TaeHoon0@users.noreply.github.com>
2024-03-27 22:33:01 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.47 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,15 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        String[] arr = s.split(" ");
+        Stack<Integer> stack = new Stack<>();
         
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i].equals("Z")) {
-                answer -= Integer.parseInt(arr[i - 1]);
-                continue;
+        for(String str : s.split(" ")) {
+            if(str.equals("Z")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(str));
             }
-            answer += Integer.parseInt(arr[i]);
+        }
+        
+        for(int i : stack) {
+            answer += i;
         }
         
         return answer;
