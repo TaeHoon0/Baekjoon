198811d6a03cc21e665686974773e9bec16d8e25
TaeHoon0 <48711163+TaeHoon0@users.noreply.github.com>
2024-03-27 21:36:49 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.26 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String my_string) {
+        my_string = my_string.replaceAll("[a-zA-Z]", " ");
+        String[] nums = my_string.split(" ");
+        int answer = 0;
+       
+        for(String num : nums) {
+            if(num.equals(" ") || num.equals("")) continue;
+            int n = Integer.parseInt(num);
+            answer += n;
+        }
+
+        return answer;
+    }
+}
