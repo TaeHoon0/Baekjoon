e44cb065cab1627a8e70c49e2e6c039a8b3f9989
TaeHoon0 <48711163+TaeHoon0@users.noreply.github.com>
2024-04-07 23:05:25 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 58.68 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+
+        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        stack.add(arr[0]);
+        list.add(arr[0]);
+        for(int n : arr) {
+            if(stack.peek() != n) {
+                stack.add(n);
+                list.add(n);
+            }
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
