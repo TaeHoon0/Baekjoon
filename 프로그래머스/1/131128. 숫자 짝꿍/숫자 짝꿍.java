9fac8e8857e24b45c8acc78fcdd7a99d3ac10d69
TaeHoon0 <48711163+TaeHoon0@users.noreply.github.com>
2024-05-06 16:38:39 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.12 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String X, String Y) {
+        HashMap<Integer, Integer> mapX = new HashMap<>();
+        HashMap<Integer, Integer> mapY = new HashMap<>();
+        StringBuilder couple = new StringBuilder();
+        
+        for(int i = 0; i < 10; i++) {
+            mapX.put(i, -1);
+            mapY.put(i, -1);
+        }
+        
+        // X, Y가 갖고 있는 수 체크
+        for(char c : X.toCharArray()) mapX.put(c - '0', mapX.getOrDefault(c - '0', 0) + 1);
+        for(char c : Y.toCharArray()) mapY.put(c - '0', mapY.getOrDefault(c - '0', 0) + 1);
+          
+        // X, Y 비교하여 List 추가
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j <= (Math.min(mapX.get(i), mapY.get(i))); j++) {
+                couple.append(i);
+            }
+        }
+        
+        if(couple.toString().equals("")) return "-1";
+        if(couple.toString().endsWith("0")) return "0";
+        return couple.reverse().toString();
+    }
+}
