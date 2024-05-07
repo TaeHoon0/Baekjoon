779df03e31a6bc4929c1eb9ba84726e0f3b7a9e6
TaeHoon0 <48711163+TaeHoon0@users.noreply.github.com>
2024-05-07 19:02:21 +0900

    [level 1] Title: 대충 만든 자판, Time: 37.69 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        
+        for(int i = 0; i < targets.length; i++) {
+            int sum = 0;    // 누른 수
+            boolean check = false; // target의 단어가 keymap에 존재하는지 체크 
+            
+            for(char c : targets[i].toCharArray()) {
+                int min = 101;  // 각 targt의 char마다 최소 누른 수
+                    
+                for(String key : keymap) {
+                    for(int j = 0; j < key.length(); j++) {
+                        if(c == key.charAt(j)) {
+                            min = Math.min(min, j + 1); // j는 0 , 클릭은 1부터 시작
+                        }
+                    }
+                }
+                
+                if(min == 101) check = true;
+                sum += min;
+            }
+            
+            if(check) answer[i] = -1;
+            else answer[i] = sum;
+        }
+        
+        
+        return answer;
+    }
+}
